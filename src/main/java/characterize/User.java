1696b1c0cb6c3afdfd4e5ec3c8d168bb9b2564a3
Polygon Unicorn <26953790+PolygonUnicorn@users.noreply.github.com>
2021-05-18 21:58:46 +0200

    Add online functionality
    Add Mongo class to create connection with database
    User class manages the current user's data

---
+++ src/main/java/characterize/User.java
@@ -0,0 +1,61 @@
+package characterize;
+
+import com.mongodb.client.MongoCollection;
+import cz.polygonunicorn.javahandysnippets.HSConsoleInput;
+import mongodb.Mongo;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public class User {
+    private String email = null;
+    private boolean loggedIn = false;
+
+    public User() {
+    }
+
+    public void register() {
+        System.out.println("Register thingy");
+
+    }
+
+    public void login() {
+        String email = HSConsoleInput.getString(Translation.getString("email"), true);
+//            char[] password = System.console().readPassword();
+        String password = HSConsoleInput.getString(Translation.getString("password"), true);
+
+        Document query = new Document("email", new Document("$eq", email));
+//        query.append("password", new Document("$eq", Arrays.toString(password)));
+        query.append("password", new Document("$eq", password));
+        MongoCollection<Document> users;
+
+        try {
+            users = Mongo.getCollection("users");
+        } catch (ExceptionInInitializerError e) {
+            System.out.println(Translation.getString("error_connection"));
+            return;
+        }
+
+        var document = users.find(query).first();
+
+        if (document != null) {
+            this.email = document.getString("email");
+            this.loggedIn = true;
+        } else {
+            System.out.println(Translation.getString("wrongLogin"));
+        }
+    }
+
+    public void logout() {
+        this.email = null;
+        this.loggedIn = false;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void logOot() {
+        loggedIn = false;
+    }
+}
---
+++ src/main/java/mongodb/Mongo.java
@@ -0,0 +1,24 @@
+package mongodb;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Mongo {
+    private static final String dbUsr = System.getenv("DATABASE_USER");
+    private static final String dbPwd = System.getenv("DATABASE_PWD");
+    private static final MongoClient mongoClient = MongoClients.create(
+            "mongodb+srv://" + dbUsr + ":" + dbPwd + "@cluster0.wzgos.mongodb.net/characterize?retryWrites=true&w=majority");
+
+    public static MongoCollection<Document> getCollection(String collectionName) {
+        return mongoClient.getDatabase("characterize").getCollection(collectionName);
+    }
+
+    static {
+//        Logger.getLogger("org.mongodb.driver").setLevel(Level.OFF);
+    }
+}
